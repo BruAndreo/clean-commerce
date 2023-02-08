7dbb5b505be2f283286acc42520c4995590d0779
Bruno Lino <brunolino2026@gmail.com>
2023-02-07 22:23:48 -0300

    test: Added test to validate product dimensions

--- test/application/Product.test.ts
+++ test/application/Product.test.ts
@@ -1,2 +1,28 @@
+import Product from "../../src/application/Product";
+import { ProductDefinition } from "../../src/types/ProductDefinition";
 
-test.todo("Não deve criar um Produto com Dimensoes negativas ou zeradas");
+const productBase: ProductDefinition =  {
+  idProduct: 3,
+  name: "Veja Desengordurante",
+  description: "Desengordutante veja com fragancia de limao",
+  price: 10,
+  height: 18,
+  width: 10,
+  depth: 5,
+  weight: 0.500
+};
+
+
+test.each([
+  [ 'largura', 'Width', { ...productBase, width: -1 } ],
+  [ 'largura', 'Width', { ...productBase, width: 0 } ],
+  [ 'altura', 'Height', { ...productBase, height: -1 } ],
+  [ 'altura', 'Height', { ...productBase, height: 0 } ],
+  [ 'peso', 'Weight', { ...productBase, weight: -1 } ],
+  [ 'peso', 'Weight', { ...productBase, weight: 0 } ],
+  [ 'profundidade', 'Depth', { ...productBase, depth: -1 } ],
+  [ 'profundidade', 'Depth', { ...productBase, depth: 0 } ]
+])("Não deve criar a instancia de um produto com dimensoes negativas ou zeradas: %p", (dimensionName, field, product) => {
+  const operation = () => new Product(product);
+  expect(operation).toThrow(`${field} can't be equal or less than zero`);
+});
